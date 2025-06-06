55588c71a8d25464c82b96017b5d2f91d8fc4078
Roscoe A. Bartlett <rabartl@sandia.gov>
2025-06-06 17:30:14 +0000

    AI: Perform the Extract Function of Teuchos::CommandLineProcessor::parse() using OpenHands
    This refactoring was performed with OpenHands using OpenAI o3-mini using the
    prompt:
    
    ----
    
    Perform the Extract Function refactoring of the parent function
    Teuchos::CommandLineProcessor::parse() in the file
    packages/teuchos/core/src/Teuchos_CommandLineProcessor.cpp to make more
    understandable and maintainable.
    
    ----
    
    I had to tell it to also update the header file (it only updated the source file
    at first).
    
    It was not given any tools to compile so it had no way to test this.  It kept
    asking me to check it.
    
    There is zero chance this will compile.
    
    This was not really agent mode at all.

--- packages/teuchos/core/src/Teuchos_CommandLineProcessor.cpp
+++ packages/teuchos/core/src/Teuchos_CommandLineProcessor.cpp
@@ -252,54 +252,79 @@ CommandLineProcessor::parse(
   ) const
 {
   add_extra_output_setup_options();
-  std::string        opt_name;
-  std::string        opt_val_str;
-  const std::string  echo_cl_opt = "echo-command-line";
-  const std::string  help_opt = "help";
-  const std::string  pause_opt = "pause-for-debugging";
   int procRank = GlobalMPISession::getRank();
 
-  // check for help options before any others as we modify
-  // the values afterwards
-  for( int i = 1; i < argc; ++i ) {
-    bool gov_return = get_opt_val( argv[i], &opt_name, &opt_val_str );
-    if( gov_return && opt_name == help_opt ) {
-      if(errout) printHelpMessage( argv[0], *errout );
+  EParseCommandLineReturn ret = handleHelpOption(argc, argv, errout);
+  if(ret == PARSE_HELP_PRINTED)
+    return PARSE_HELP_PRINTED;
+
+  ret = processOptions(argc, argv, errout, procRank);
+  if(ret != PARSE_SUCCESSFUL)
+    return ret;
+
+  ret = checkRequiredOptions(argv[0], errout);
+  if(ret != PARSE_SUCCESSFUL)
+    return ret;
+
+  setupDefaultOutput();
+
+  return PARSE_SUCCESSFUL;
+}
+
+
+void CommandLineProcessor::printHelpMessage( const char program_name[],
+/* Extracted helper functions for parse() */
+
+EParseCommandLineReturn CommandLineProcessor::handleHelpOption(int argc, char* argv[], std::ostream* errout) const {
+  std::string opt_name;
+  std::string opt_val_str;
+  const std::string help_opt = "help";
+  for (int i = 1; i < argc; ++i) {
+    bool gov_return = get_opt_val(argv[i], &opt_name, &opt_val_str);
+    if (gov_return && opt_name == help_opt) {
+      if (errout) printHelpMessage(argv[0], *errout);
       return PARSE_HELP_PRINTED;
     }
   }
-  // check all other options
-  for( int i = 1; i < argc; ++i ) {
-    bool gov_return = get_opt_val( argv[i], &opt_name, &opt_val_str );
-    if( !gov_return ) {
-      if(procRank == 0)
-        print_bad_opt(i,argv,errout);
-      if( recogniseAllOptions() )
+  return PARSE_SUCCESSFUL;
+}
+
+EParseCommandLineReturn CommandLineProcessor::processOptions(int argc, char* argv[], std::ostream* errout, int procRank) const {
+  std::string opt_name;
+  std::string opt_val_str;
+  const std::string echo_cl_opt = "echo-command-line";
+  const std::string pause_opt = "pause-for-debugging";
+
+  for (int i = 1; i < argc; ++i) {
+    bool gov_return = get_opt_val(argv[i], &opt_name, &opt_val_str);
+    if (!gov_return) {
+      if (procRank == 0)
+        print_bad_opt(i, argv, errout);
+      if (recogniseAllOptions())
         return PARSE_UNRECOGNIZED_OPTION;
-      else {
+      else
         continue;
-      }
     }
-    if( opt_name == echo_cl_opt ) {
-      if(errout && procRank == 0) {
+    if (opt_name == echo_cl_opt) {
+      if (errout && procRank == 0) {
         *errout << "\nEchoing the command-line:\n\n";
-        for( int j = 0; j < argc; ++j )
+        for (int j = 0; j < argc; ++j)
           *errout << argv[j] << " ";
         *errout << "\n\n";
       }
       continue;
     }
-    if( opt_name == pause_opt ) {
+    if (opt_name == pause_opt) {
 #ifndef _WIN32
       Array<int> pids;
       pids.resize(GlobalMPISession::getNProc());
       int rank_pid = getpid();
-      GlobalMPISession::allGather(rank_pid,pids());
-      if(procRank == 0)
-        for (int k=0; k<GlobalMPISession::getNProc(); k++)
+      GlobalMPISession::allGather(rank_pid, pids());
+      if (procRank == 0)
+        for (int k = 0; k < GlobalMPISession::getNProc(); ++k)
           std::cerr << "Rank " << k << " has PID " << pids[k] << std::endl;
 #endif
-      if(procRank == 0) {
+      if (procRank == 0) {
         std::cerr << "\nType 0 and press enter to continue : ";
         int dummy_int = 0;
         std::cin >> dummy_int;
@@ -307,21 +332,18 @@ CommandLineProcessor::parse(
       GlobalMPISession::barrier();
       continue;
     }
-    // Lookup the option (we had better find it!)
-    options_list_t::iterator  itr = options_list_.find(opt_name);
-    if( itr == options_list_.end() ) {
-      if(procRank == 0)
-        print_bad_opt(i,argv,errout);
-      if( recogniseAllOptions() )
+    options_list_t::iterator itr = options_list_.find(opt_name);
+    if (itr == options_list_.end()) {
+      if (procRank == 0)
+        print_bad_opt(i, argv, errout);
+      if (recogniseAllOptions())
         return PARSE_UNRECOGNIZED_OPTION;
       else
         continue;
     }
-    // Changed access to second value of std::map to not use overloaded arrow operator,
-    // otherwise this code will not compile on Janus (HKT, 12/01/2003)
-    opt_val_val_t &opt_val_val = (*itr).second;
+    opt_val_val_t &opt_val_val = itr->second;
     opt_val_val.was_read = true;
-    switch( opt_val_val.opt_type ) {
+    switch (opt_val_val.opt_type) {
       case OPT_BOOL_TRUE:
         *(any_cast<bool*>(opt_val_val.opt_val)) = true;
         break;
@@ -329,61 +351,56 @@ CommandLineProcessor::parse(
         *(any_cast<bool*>(opt_val_val.opt_val)) = false;
         break;
       case OPT_INT:
-        *(any_cast<int*>(opt_val_val.opt_val)) = asSafe<int> (opt_val_str);
+        *(any_cast<int*>(opt_val_val.opt_val)) = asSafe<int>(opt_val_str);
         break;
       case OPT_LONG_INT:
-        *(any_cast<long int*>(opt_val_val.opt_val)) = asSafe<long int> (opt_val_str);
+        *(any_cast<long int*>(opt_val_val.opt_val)) = asSafe<long int>(opt_val_str);
         break;
       case OPT_SIZE_T:
-        *(any_cast<size_t *>(opt_val_val.opt_val)) = asSafe<size_t> (opt_val_str);
+        *(any_cast<size_t*>(opt_val_val.opt_val)) = asSafe<size_t>(opt_val_str);
         break;
       case OPT_LONG_LONG_INT:
-        *(any_cast<long long int*>(opt_val_val.opt_val)) = asSafe<long long int> (opt_val_str);
+        *(any_cast<long long int*>(opt_val_val.opt_val)) = asSafe<long long int>(opt_val_str);
         break;
       case OPT_DOUBLE:
-        *(any_cast<double*>(opt_val_val.opt_val)) = asSafe<double> (opt_val_str);
+        *(any_cast<double*>(opt_val_val.opt_val)) = asSafe<double>(opt_val_str);
         break;
       case OPT_FLOAT:
-        *(any_cast<float*>(opt_val_val.opt_val)) = asSafe<float> (opt_val_str);
+        *(any_cast<float*>(opt_val_val.opt_val)) = asSafe<float>(opt_val_str);
         break;
       case OPT_STRING:
         *(any_cast<std::string*>(opt_val_val.opt_val)) = remove_quotes(opt_val_str);
         break;
       case OPT_ENUM_INT:
-        if( !set_enum_value( i, argv, opt_name, any_cast<int>(opt_val_val.opt_val),
-            remove_quotes(opt_val_str), errout ) )
-        {
+        if (!set_enum_value(i, argv, opt_name, any_cast<int>(opt_val_val.opt_val),
+                             remove_quotes(opt_val_str), errout))
           return PARSE_UNRECOGNIZED_OPTION;
-        }
         break;
       default:
-        TEUCHOS_TEST_FOR_EXCEPT(true); // Local programming error only
+        TEUCHOS_TEST_FOR_EXCEPT(true);
     }
   }
-  // Look for options that were required but were not set
-  for(
-    options_list_t::const_iterator itr = options_list_.begin();
-    itr != options_list_.end();
-    ++itr
-    )
-  {
-    const opt_val_val_t   &opt_val_val  = (*itr).second;
-    if( opt_val_val.required && !opt_val_val.was_read ) {
-      const std::string     &opt_val_name = (*itr).first;
-#define CLP_ERR_MSG \
-      "Error, the option --"<<opt_val_name<<" was required but was not set!"
-      if(errout)
-        *errout << std::endl << argv[0] << " : " << CLP_ERR_MSG << std::endl;
-      if( throwExceptions() ) {
-        TEUCHOS_TEST_FOR_EXCEPTION( true, ParseError, CLP_ERR_MSG );
-      }
+  return PARSE_SUCCESSFUL;
+}
+
+EParseCommandLineReturn CommandLineProcessor::checkRequiredOptions(const char* programName, std::ostream* errout) const {
+  for (options_list_t::const_iterator itr = options_list_.begin();
+       itr != options_list_.end(); ++itr) {
+    const opt_val_val_t &opt_val_val = itr->second;
+    if (opt_val_val.required && !opt_val_val.was_read) {
+      const std::string &opt_val_name = itr->first;
+      if (errout)
+        *errout << std::endl << programName << " : Error, the option --" << opt_val_name << " was required but was not set!" << std::endl;
+      if (throwExceptions())
+        TEUCHOS_TEST_FOR_EXCEPTION(true, ParseError, "Error, the option --" << opt_val_name << " was required but was not set!");
       return PARSE_ERROR;
-#undef CLP_ERR_MSG
     }
   }
-  // Set the options of a default stream exists and if we are asked to
-  RCP<FancyOStream>
-    defaultOut = VerboseObjectBase::getDefaultOStream();
+  return PARSE_SUCCESSFUL;
+}
+
+void CommandLineProcessor::setupDefaultOutput() const {
+  RCP<FancyOStream> defaultOut = VerboseObjectBase::getDefaultOStream();
   if (defaultOut.get() && addOutputSetupOptions_) {
     if (output_all_front_matter_ != output_all_front_matter_default_)
       defaultOut->setShowAllFrontMatter(output_all_front_matter_);
@@ -397,11 +414,9 @@ CommandLineProcessor::parse(
       defaultOut->setOutputToRootOnly(output_to_root_rank_only_);
     RCPNodeTracer::setPrintRCPNodeStatisticsOnExit(print_rcpnode_statistics_on_exit_);
   }
-  return PARSE_SUCCESSFUL;
 }
 
 
-void CommandLineProcessor::printHelpMessage( const char program_name[],
   std::ostream &out ) const
 {
   add_extra_output_setup_options();
